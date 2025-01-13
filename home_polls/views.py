32c21de9d3412ba88d5948d996681f85b3ad00a8
reza <yadegarireza50@googlemail.com>
2025-01-13 22:41:41 +0330

    for views.py some codes writed

--- home_polls/views.py
+++ home_polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PollForm
 
 # Create your views here.
+def home_poll(request):
+    if request.method == 'POST':
+        form = PollForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') #replace it after termin with fardin
+    else:
+        form = PollForm()
+    return render(request, '',{'form'.form})        
