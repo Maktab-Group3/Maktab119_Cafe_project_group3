74eb24cc15167229211eaae8f14f331709d265b4
reza <yadegarireza50@googlemail.com>
2025-01-13 22:15:12 +0330

    models for polls writed

--- home_polls/models.py
+++ home_polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Polls(models.Model):
+    name = models.CharField(max_length=30,null=False)
+    email = models.EmailField(unique=True,null=False)
+    message = models.TextField(max_length=250)
