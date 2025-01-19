9f1b15f3f36da141ba483b400ebe28a4fb3b14e8
h-alizadeh2 <hajar466@gmail.com>
2025-01-19 20:37:34 +0330

    write views file

--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import MenuItem, Category
 
 # Create your views here.
+def category(request):
+    category = MenuItem.objects.filter('category' = category)
+    return render(request, 'category.html', {'category':category})
+def menuitem(request,pk):
+    menuitem = MenuItem.objects.get(id=pk)
+    return render(request, 'menuitem.html', {'menuitem':menuitem})
