073f50bb80b527f4029a4b8d5699693f317b9471
reza <yadegarireza50@googlemail.com>
2025-01-22 20:10:12 +0330

    orders was chenged

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from tkinter import CASCADE
-from menu_items.models import menu_item
+from menu_items.models import MenuItem
 from table.models import table
 from user.models import user 
 from receipts.models import receipt
+from users.models import User
 
 
 class TimeStampedModel(models.Model):
@@ -13,9 +14,9 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-class Order(TimeStampedModel):
+class Order(models.Model,TimeStampedModel):
     
-    menu_item = menu_item.models.ForeignKey('menu_item', on_delete=models.CASCADE, name='menu_item')
+    menu_item = MenuItem.models.ForeignKey('menu_item', on_delete=models.CASCADE, name='menu_item')
     table = table.models.ForeignKey('table', on_delete=models.CASCADE, name='table')
     user = user.models.ForeignKey('user', on_delete=models.CASCADE, name='user') #
     recipt= recipt.models.ForeignKey('recipt', on_delete=models.CASCADE, name='recipt')
@@ -36,7 +37,7 @@ class Order(TimeStampedModel):
         ('Canceled','Canceled')
         
     ]
-    
+    TimeStampedModel=TimeStampedModel.
    
     take_a_way =models.BooleanField(default= False)
 
@@ -48,14 +49,16 @@ class Order(TimeStampedModel):
     def __str__(self):
         return f"{self.created_at},{self.updated_at}"
 
-#parsa
-class Receipt(TimeStampedModel):
-    total_price = models.DecimalField(max_digits=10, decimal_places=5)
-    is_refunded = models.BooleanField(default=False)
-    order = models.OneToOneField(Order, on_delete=models.CASCADE)
-    
+
+
+#this codes belong to the reza
+
+class CartItem(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return f'{self.total_price}'
-   
-  
-       
+        return f"{self.menu_item.name} X {self.quantity}"   
+    
