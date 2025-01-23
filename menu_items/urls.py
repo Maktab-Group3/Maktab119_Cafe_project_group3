4217282ed84ae9e536ee6604b18e88bd78650850
reza <yadegarireza50@googlemail.com>
2025-01-23 21:31:03 +0330

    for cookies and test it writed some path

--- menu_items/urls.py
+++ menu_items/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 #    path('menu_item/<int:pk>', views.menuitem, name='menuitem' )
-    path('menu/', views.show_all_menu, name = 'all menu')
+    path('menu/', views.show_all_menu, name = 'all menu'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('api/menu_item/<int:menu_item_id>/', views.menu_item_api, name='menu_item_api'),
+    path('cookie/',views.cookie_back)
         
 ]
