97309133269a5b07d3a830e0df61cba8d5cee2f5
reza <yadegarireza50@googlemail.com>
2025-01-25 11:06:07 +0330

    its changed

--- src/urls.py
+++ src/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 #    path('',include('homes.urls')),
-    path('', include('menu_items.urls'), name = 'menuitem' )
+    path('', include('menu_items.urls'), name = 'menu' )
     
     
     
