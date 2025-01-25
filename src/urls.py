00e01da05c7a0ec2b444233205e63ece374ce2bf
Reza <yadegarireza50@googlemail.com>
2025-01-25 22:52:00 +0330

    Update urls.py for remove conflicts

--- src/urls.py
+++ src/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('homes.urls')),
-    path('', include('menu_items.urls'), name = 'menuitem' )
+#    path('',include('homes.urls')),
+    path('', include('menu_items.urls'), name = 'menu' )
 ]
