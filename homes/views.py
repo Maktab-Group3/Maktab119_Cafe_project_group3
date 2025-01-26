3ffe7ed7d4220b0711d0ee0fb2f6fa1245545dec
fardinebadi <fardinebadi363@gmail.com>
2025-01-26 05:44:33 -0500

    style:(create menu page and order page use inhertance templates)

--- homes/views.py
+++ homes/views.py
@@ -3,21 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 def show_home(request):
     return render(request,'base.html')
+def show_menu(request):
+    return render(request,'menu.html')
+
+def show_order(request):
+    return render(request,'order.html')
 
 
-
-
-
-from django.shortcuts import render, redirect
-from .forms import PollForm
+# from django.shortcuts import render, redirect
+# from .forms import PollForm
 
 # Create your views here.
-def home_poll(request):
-    if request.method == 'POST':
-        form = PollForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home') #replace it after termin with fardin
-    else:
-        form = PollForm()
-    return render(request, '',{'form'.form})     
+# def home_poll(request):
+#     if request.method == 'POST':
+#         form = PollForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home') #replace it after termin with fardin
+#     else:
+#         form = PollForm()
+#     return render(request, '',{'form'.form})     
