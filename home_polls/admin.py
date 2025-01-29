f584514e904a3cd74d1648ed734a2e38d8d783ac
reza <yadegarireza50@googlemail.com>
2025-01-29 11:50:26 +0330

    admin and registered

--- home_polls/admin.py
+++ home_polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import MenuItem, Category
 
 # Register your models here.
+
+class AdminMenu(admin.ModelAdmin):
+    list_display = ["name","price"]
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ["name"]
+
+admin.site.register(Category)
+admin.site.register(MenuItem)
