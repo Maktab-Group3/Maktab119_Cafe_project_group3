3da485a8e83ed8df8b3780a8de7df7dad90567df
reza <yadegarireza50@googlemail.com>
2025-01-30 11:12:17 +0330

    urls modified

--- homes/urls.py
+++ homes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from homes.views import show_home,show_menu,show_order
 urlpatterns = [
-    path('',show_home,name="home_cafe"),
+    path('',show_home,name="home"),
 #    path('menu/',show_menu,name="show menu"),
 #    path('order/',show_order,name="show_order")
    
