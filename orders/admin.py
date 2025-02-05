f91d3e6a3218a195e0c8fc312ed0ac6c384fe376
parsafk010 <parsafakharian.f33@gmail.com>
2025-02-05 22:00:01 +0330

    crate my admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Receipt
 
-# Register your models here.
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'total_price', 'is_refunded','status_R')  
+    search_fields = ['id', 'order','status_R']
+    list_filter = ('status_R',)
+    fieldsets = [
+        ('Orders', {'fields': ['order']}),  
+        ('Total Price', {'fields': ['total_price']}),  
+        ('Refund Status', {'fields': ['is_refunded']}), 
+        ('Status Receipt' , {'fields': ['status_R']}), 
+    ]
+    
+   
+      
+
+
+
+admin.site.register(Receipt, ReceiptAdmin) 
