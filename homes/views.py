41d0db0e5b661c84915688accff8fa3b2aabc8e2
fardinebadi <fardinebadi363@gmail.com>
2025-02-05 05:43:53 -0500

    fix:(add function for fix in config)

--- homes/views.py
+++ homes/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
-
-# Create your views here.
+def show_home(request):
+    return render(request,'home_reza_sample.html')
+def show_menu(request):
+    return render(request,'menu.html')
 
 def show_home(request):
     data_category=Category.objects.all()
